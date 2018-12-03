1d5cdd23c5b66d2661219420aeab81c02ecdaf3f
ChrisBrinton <cbrinton@gmail.com>
2018-12-03 18:29:27 +0000

    Optimize cursor query, add stop and trim scripts

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask_cors import CORS
+import pymongo
 from pymongo import MongoClient
 import json
 import datetime as dt
@@ -10,11 +11,14 @@ print('connecting to mongo...')
 client = MongoClient('localhost', 27017)  # make this explicit
 db = client['gdtechdb_prod']
 #db = client.test
-coll = db['Sensors']
+sensors = db['Sensors']
+sensorsLatest = db['SensorsLatest']
 app = Flask(__name__)
 CORS(app)
 timefmt = '%Y-%m-%d %H:%M:%S'
 
+def cleanvalue(value):
+    return float(value.replace('b', '').replace('v', '').replace("'", ""))
 
 @app.route("/", methods=['GET'])
 def hello():
@@ -27,14 +31,14 @@ def hello():
 
 @app.route("/stats", methods=['GET'])
 def stats():
-    ct = coll.count()
+    ct = sensors.count()
     return 'total rows:' + str(ct)
 
 
 @app.route('/sensorlist', methods=['GET'])
 def sensorlist():
     print('sensorlist returning...')
-    d = coll.distinct('node_id') 
+    d = sensors.distinct('node_id') 
     print(json.dumps(d))
     return json.dumps(d)
 
@@ -46,16 +50,15 @@ def today():
 
 
 def getstart(p):
-    # p should be a number specifying the delta in days.
+    # p should be a number specifying the delta in hours.
     nowdatetime = dt.datetime.now(tzutc())
     if p is None:
-        diff = dt.timedelta(days=1)
+        diff = dt.timedelta(hours=24)
     else:
-        diff = dt.timedelta(days=int(p))
+        diff = dt.timedelta(hours=int(p))
 
     start = nowdatetime - diff
     return start.timestamp()
-
 
 @app.route("/sensor/<node>", methods=['GET'])
 def people(node):
@@ -67,23 +70,97 @@ def people(node):
     except ValueError as err:
         print('invalid skip parameter %s. defaulting.' % skip)
         skip = 0
+    try:
+        period = int(period)*24
+    except ValueError as err:
+        period = 24
     start = getstart(period)
     docs = getdata(node, start, skip, type)
     return json.dumps(docs)
 
-@app.route("/gw/<gw>/<int:node>", methods=['GET'])
-def gw(gw, node):
+@app.route("/nodelist/<gw>", methods=['GET'])
+def nodelist(gw):
+    period = request.args.get('period')
+    try:
+        period = int(period)*24
+    except TypeError as err:
+        period = 24
+    start = getstart(period)
+    values = getnodelist(gw, start)
+    return json.dumps(sorted(values))
+
+def getnodelist(gw, start):
+    qry = {'gateway_id': gw, 'time': {'$gte': start}}
+    print('query is %s' % qry)
+    print('starting query at ', dt.datetime.now())
+    values = sensorsLatest.distinct('node_id', qry)
+    print('query returned at ', dt.datetime.now()) #this takes 16 millis
+    print(json.dumps(sorted(values)))
+    return values
+
+@app.route("/latest/<gw>", methods=['GET'])
+def latest(gw):
+    period = request.args.get('period','')
+    try:
+        period = int(period)
+    except ValueError as err:
+        period = 1
+    start=getstart(period)
+    values = getlatest(gw, start)
+    print(json.dumps(values))
+    return json.dumps(values)
+
+def getlatest(gw, start):
+    docs = []
+    qry = {'gateway_id': gw, 'time': {'$gte':int(start)}}
+    sortparam = [('node_id', -1)]
+    print('query is ', qry, ' sort is ', sortparam)
+    cursor = sensorsLatest.find(qry).sort(sortparam)
+    print('query run')
+    for doc in cursor:
+        newdoc = { 'node_id': 0, 'type': '', 'value': 0, 'human_time': '', 'time': 0 }
+        newdoc['value'] = float(doc['value'].replace('b', '').replace('v', '').replace("'", ""))
+        newdoc['human_time'] = dt.datetime.fromtimestamp(doc['time']).strftime(timefmt)
+        newdoc['time'] = doc['time']
+        newdoc['type'] = doc['type']
+        newdoc['node_id'] = doc['node_id']
+        docs.append(newdoc)
+
+    print(json.dumps(docs))
+    return docs
+
+@app.route("/gw/<gw>", methods=['GET'])
+def gw(gw):
+    nodes = request.args.getlist('node')
     type = request.args.get('type', '')
     period = request.args.get('period')
     timezone = request.args.get('timezone','None')
-    skip = 0
+    returndocs = []
+    try:
+        period = int(period)*24
+    except ValueError as err:
+        period = 24
+    print('calling gwiteratenodes with ', gw, nodes, type, period, timezone, ' at ', dt.datetime.now())
+    returndocs = gwiteratenodes(gw, nodes, type, period, timezone)
+    return json.dumps(returndocs)
+
+def gwiteratenodes(gw, nodes, type, period, timezone):
     start = getstart(period)
-    docs = getdatausinggw(gw, node, start, type, timezone)
-    return json.dumps(docs)
+    returndocs = []
+    for node in nodes:
+        record = {'nodeID': 0, 'sensorData': []}
+        record['nodeID'] = node
+        print('calling gwdatausinggw with ', gw, node, start, type, timezone, dt.datetime.now())
+        record['sensorData'] = getdatausinggw(gw, node, start, type, timezone) 
+        print('finished getdatausinggw for', gw, node, start, type, timezone, dt.datetime.now())
+        returndocs.append(record)
+    return returndocs
 
 def getdatausinggw(gw, node, start, mytype, timezone):
-    print('getdatausinggw starting...')
+    print('getdatausinggw starting. C extentions in use:', pymongo.has_c())
     docs = []
+    resultsarray = []
+    empty_results = {'results': '0'}
 
     try:
         toZone = gettz(timezone)
@@ -97,65 +174,64 @@ def getdatausinggw(gw, node, start, mytype, timezone):
     if mytype:
         qry['type'] = mytype
     print('query is %s and sort is ' % qry, sortparam)
-    count = coll.find(qry).count()
-    print('expecting %i records' % count);
-    cursor = coll.find(qry).sort(sortparam)
-    print('query run.')  
+    print('starting query at ', dt.datetime.now())
+    cursor = sensors.find(qry).sort(sortparam).batch_size(100000)
+    print('query returned at ', dt.datetime.now()) #this takes .2 millis
+    for row in cursor:
+        resultsarray.append(row)
+    print('Arrayified at ', dt.datetime.now())
+    count = len(resultsarray)
+    print('%i records returned' % count, dt.datetime.now()); #this takes 1.86 sec
     ct = 0
     total = 0
     skip=0
+    if count == 0:
+        return empty_results
     if count > 300:
         skip = int(count/300 +.49)
-        print('Since more than 300 records were returned, skip is set to %i' % skip)
-    for doc in cursor:
+        print('Since more than 300 records were returned, skip is set to %i' % skip, dt.datetime.now()) #this take .1 milli
+
+    #insert initial doc as first 'goalpost' with time same as start
+    newdoc = {'value': 0, 'human_time': '', 'time': 0}
+    # datetimes in DB are utc, convert to local timezone
+    newdoc['human_time'] = dt.datetime.fromtimestamp(start).replace(tzinfo=fromZone).astimezone(toZone).strftime(timefmt)
+    newdoc['time'] = start
+    newvalue = cleanvalue(resultsarray[skip+1]['value'])
+    newdoc['value'] = newvalue
+    docs.append(newdoc)
+    
+    for doc in resultsarray:
         total += 1
         ct += 1
+        newdoc = {'value': 0, 'human_time': '', 'time': 0}
         # skip if needed
         if ct > skip:
-            doc['_id'] = str(doc['_id'])  # serialization support
-            doc['value'] = float(doc['value'].replace('b', '').replace('v', '').replace("'", ""))
+            #newdoc['value'] = float(doc['value'].replace('b', '').replace('v', '').replace("'", ""))
+            newvalue = cleanvalue(doc['value'])
+            newdoc['value'] = newvalue
+            latestvalue = newvalue
             # datetimes in DB are utc, convert to local timezone
-            doc['human_time'] = dt.datetime.fromtimestamp(doc['time']).replace(tzinfo=fromZone).astimezone(toZone).strftime(timefmt)
-            if 'iso_time' in doc:
-                doc['iso_time'] = str(doc['iso_time'])
-            docs.append(doc)
+            newdoc['human_time'] = dt.datetime.fromtimestamp(doc['time']).replace(tzinfo=fromZone).astimezone(toZone).strftime(timefmt)
+            newdoc['time'] = doc['time']
+            docs.append(newdoc)
             ct = 0
     if ct !=0:
-        doc['_id'] = str(doc['_id'])  # serialization support
-        doc['value'] = float(doc['value'].replace('b', '').replace('v', '').replace("'", ""))
-        doc['human_time'] = dt.datetime.fromtimestamp(doc['time']).replace(tzinfo=fromZone).astimezone(toZone).strftime(timefmt)
-        if 'iso_time' in doc:
-            doc['iso_time'] = str(doc['iso_time'])
-        docs.append(doc)
+        newdoc['value'] = float(doc['value'].replace('b', '').replace('v', '').replace("'", ""))
+        # datetimes in DB are utc, convert to local timezone
+        newdoc['human_time'] = dt.datetime.fromtimestamp(doc['time']).replace(tzinfo=fromZone).astimezone(toZone).strftime(timefmt)
+        newdoc['time'] = doc['time']
+        docs.append(newdoc)
 
-    format_hour = "%I:%M%p"
-    format_day = "%b %d"
-    numofpips = 7
-    xvalues = dict()
-    for i in range(numofpips-1):
-        chunk = int((len(docs)-1)/(numofpips-1))
-        datestr = dt.datetime.fromtimestamp(docs[chunk*i]['time']).replace(tzinfo=fromZone).astimezone(toZone).strftime(format_day) 
-        if datestr[0] == '0':
-            datestr = datestr[1:]
-        timestr = dt.datetime.fromtimestamp(docs[chunk*i]['time']).replace(tzinfo=fromZone).astimezone(toZone).strftime(format_hour)
-        if timestr[0] == '0':
-            timestr = timestr[1:]
-        xvalues[i] = {
-        'date': datestr,
-        'time': timestr
-        }
-    datestr = dt.datetime.fromtimestamp(docs[len(docs)-1]['time']).replace(tzinfo=fromZone).astimezone(toZone).strftime(format_day) 
-    if datestr[0] == '0':
-        datestr = datestr[1:]
-    timestr = dt.datetime.fromtimestamp(docs[len(docs)-1]['time']).replace(tzinfo=fromZone).astimezone(toZone).strftime(format_hour)
-    if timestr[0] == '0':
-        timestr = timestr[1:]
-    xvalues[numofpips] = {
-    'date': datestr,
-    'time': timestr
-    }
-    docs.insert(0, xvalues)
+    #insert last doc as end 'goalpost' with timestamp of now
+    newdoc = {'value': 0, 'human_time': '', 'time': 0}
+    # datetimes in DB are utc, convert to local timezone
+    newdoc['human_time'] = dt.datetime.timestamp(dt.datetime.now())
+    newdoc['time'] = dt.datetime.timestamp(dt.datetime.now())
+    newdoc['value'] = latestvalue
+    docs.append(newdoc)
+    
     print('total docs found:', total, ' and returning:', len(docs))
+    print('document returned at ', dt.datetime.now()) #this takes 2.28 sec
     return docs
 
 def getdata(node, start, skip, mytype):
@@ -167,7 +243,7 @@ def getdata(node, start, skip, mytype):
     if mytype:
         qry['type'] = mytype
     print('query is %s and sort is ' % qry, sortparam)
-    cursor = coll.find(qry).sort(sortparam)
+    cursor = sensors.find(qry).sort(sortparam)
     print('query run.')  
     ct = 0
     total = 0
@@ -189,9 +265,55 @@ def getdata(node, start, skip, mytype):
     print('total docs found:', total, ' and returning:', len(docs))
     return docs
 
-if __name__ == "__main__":
-   #app.run(host="0.0.0.0", port=5000)
-   d = getdatausinggw('16E542',42,1519344000,'F','EST5EDT')
-   print(json.dumps(d))
-    
+def testquery():
+    docs = []
+    qry = {'gateway_id': '16E542'}
+    sortparam = [('node_id', -1)]
+    cursor = sensorsLatest.find(qry).sort(sortparam)
+    for doc in cursor:
+        doc['_id'] = str(doc['_id'])  # serialization support
+        doc['value'] = float(doc['value'].replace('b', '').replace('v', '').replace("'", ""))
+        doc['human_time'] = dt.datetime.fromtimestamp(doc['time']).strftime(timefmt)
+        if 'iso_time' in doc:
+            doc['iso_time'] = str(doc['iso_time'])
+        docs.append(doc)
 
+    print(json.dumps(docs))
+    return json.dumps(docs)
+
+def testquery1():
+    returndocs = []
+    nodes = ['42', '50']
+    for node in nodes:
+        record = {'nodeID': 0, 'sensorData': []}
+        record['nodeID'] = node
+        record['sensorData'] = getdatausinggw('16E542', node, 1519344000, 'F', 'EST5EDT') 
+        returndocs.append(record)
+    print(json.dumps(returndocs))
+    return(json.dumps(returndocs))
+
+def testquery2():
+    d = getnodelist('16E542',1519344000)
+
+def testquery3():
+    period=1
+    start=getstart(int(period)*24)
+    d = getdatausinggw('16E542','72',start,'F','EST5EDT')
+    print(json.dumps(d))
+
+def testquery4():
+    start=getstart(1)
+    d = getlatest('16E542',start)
+    print(json.dumps(d))
+
+def testquery5():
+    nodes = ['70', '72']
+    returndocs = []
+    starttime = dt.datetime.now(tzutc())
+    returndocs = gwiteratenodes('16E542', nodes, 'F', 24, 'EST5EDT')
+    endtime = dt.datetime.now(tzutc())
+    print('executed in ', endtime - starttime)
+
+if __name__ == "__main__":
+    values = testquery5() 
+    
